a2cc960cfde3bacaa3fdb366643a06d7704bd92d
ivoorusso <ivan@setandforget.io>
2023-10-04 13:18:56 -0300

    test

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,4 @@ export { default as FilteredTable } from './FilteredTable';
 export { default as MainForm } from './MainForm';
 export { default as ProgressBar } from './ProgressBar';
 export { default as Spinner } from './Spinner';
-export { default as Providers } from './providers';
 
